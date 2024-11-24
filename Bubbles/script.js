61d8711b1ef9101a09ad8d76aa522b816da70d54
CyTs <cy123.tsai@gmail.com>
2024-11-23 21:20:45 -0500

    still a mess - just trying to make sure the bubbles dont overlap with other elements

--- Bubbles/script.js
+++ Bubbles/script.js
@@ -1,7 +1,7 @@
 // Create clock element
 const clock = document.createElement('div');
 clock.id = 'clock';
-clock.className = 'clock';
+clock.style.cssText = 'position: fixed; top: 20px; left: 50%; transform: translateX(-50%);';
 document.querySelector('.container').appendChild(clock);
 
 // Update clock
@@ -22,74 +22,51 @@ let todos = JSON.parse(localStorage.getItem('todos')) || [];
 
 function saveTodos() {
     localStorage.setItem('todos', JSON.stringify(todos));
+    todoCont = document.querySelector('#todoContainer').getBoundingClientRect();
 }
 
-const cont = document.querySelector('.container');
 var clockCont = document.querySelector('#clock').getBoundingClientRect();
 var todoCont = document.querySelector('#todoContainer').getBoundingClientRect();
 
-// Draw bounding box for todoCont
-var todoBoundingBox = document.createElement('div');
-todoBoundingBox.style.position = 'absolute';
-todoBoundingBox.style.top = `${todoCont.top}px`;
-todoBoundingBox.style.left = `${todoCont.left}px`;
-todoBoundingBox.style.width = `${todoCont.width}px`;
-todoBoundingBox.style.height = `${todoCont.height}px`;
-todoBoundingBox.style.border = '2px solid red';
-cont.appendChild(todoBoundingBox);
-
-// Draw bounding box for clockCont
-var clockBoundingBox = document.createElement('div');
-clockBoundingBox.style.position = 'absolute';
-clockBoundingBox.style.top = `${clockCont.top}px`;
-clockBoundingBox.style.left = `${clockCont.left}px`;
-clockBoundingBox.style.width = `${clockCont.width}px`;
-clockBoundingBox.style.height = `${clockCont.height}px`;
-clockBoundingBox.style.border = '2px solid blue';
-cont.appendChild(clockBoundingBox);
-
 class Bubble {
-    constructor(text) {
+    constructor(text, container) {
         this.element = document.createElement('div');
         this.element.className = 'bubble';
         this.element.textContent = text;
-        this.element.style.width = `${text.length * 15}px`; // Assuming 15px per character
-        var bubCont = this.element.getBoundingClientRect();
-
-        var randomX = Math.floor(Math.random() * (cont.innerWidth - 100)) + 50;
-        var randomY = Math.floor(Math.random() * (cont.innerHeight - 100)) + 50;
-        /*
-        if(randomX + bubX > todoCont.left && randomY < (todoCont.left + todoCont.width) && 
-            randomY + bubY > todoCont.top && randomY < todoCont.bottom){
-            while(randomY + bubY > todoCont.top && randomY < todoCont.bottom){
-                randomY = Math.floor(Math.random() * (cont.innerHeight - 100)) + 50;
-            }
-        }else if(randomX + bubX > clockCont.left && randomY < (clockCont.left+clockCont.width) && randomY < (todoCont.top + clockCont.heigth)){
-            while(randomY < todoCont.bottom){
-                randomY = Math.floor(Math.random() * (cont.innerHeight - 100)) + 50;
-            }
+        
+        // Random position avoiding center third
+        const centerThirdStartX = window.innerWidth * 2 / 5;
+        const centerThirdEndX = (window.innerWidth * 3) / 5;
+        const centerThirdStartY = window.innerHeight / 3;
+        const centerThirdEndY = (window.innerHeight * 2) /3;
+        
+        // Generate x position in left or right third
+        let x;
+        if (Math.random() < 0.5) {
+            x = Math.random() * (centerThirdStartX - 150); // Left third
+        } else {
+            x = centerThirdEndX + Math.random() * (window.innerWidth - centerThirdEndX - 150); // Right third
         }
-        */
-
-        this.element.style.top = `${randomY}px`;
-        this.element.style.left = `${randomX}px`;
-        cont.appendChild(this.element);
-
-        const bubbleBox = document.createElement('div');
-        bubbleBox.style.position = 'absolute';
-        bubbleBox.style.top = `${bubCont.top}px`;
-        bubbleBox.style.left = `${bubCont.left}px`;
-        bubbleBox.style.width = `${bubCont.width}px`;
-        bubbleBox.style.height = `${bubCont.height}px`;
-        bubbleBox.style.border = '2px solid green';
-        cont.appendChild(bubbleBox);
-
+        
+        // Generate y position in top or bottom third
+        let y;
+        if (Math.random() < 0.5) {
+            y = Math.random() * (centerThirdStartY - 150); // Top third
+        } else {
+            y = centerThirdEndY + Math.random() * (window.innerHeight - centerThirdEndY - 150); // Bottom third
+        }
+        
+        this.element.style.left = `${x}px`;
+        this.element.style.top = `${y}px`;
+        
+        container.appendChild(this.element);
     }
 }
-
+// Create bubble container
+const bubblesContainer = document.getElementById('bubblesContainer');
 // Function to create new bubble
 function createBubble(text) {
-    new Bubble(text);
+    new Bubble(text, bubblesContainer);
 }
 
 function createTodoElement(todo) {
@@ -100,22 +77,6 @@ function createTodoElement(todo) {
         <span contenteditable="true">${todo.text}</span>
         <button class="delete-todo">×</button>
     `;
-
-    clockCont = document.querySelector('#clock').getBoundingClientRect();
-    todoCont = document.querySelector('#todoContainer').getBoundingClientRect();
-    // Draw bounding box for todoCont
-    todoBoundingBox.style.top = `${todoCont.top}px`;
-    todoBoundingBox.style.left = `${todoCont.left}px`;
-    todoBoundingBox.style.width = `${todoCont.width}px`;
-    todoBoundingBox.style.height = `${todoCont.height}px`;
-
-    // Draw bounding box for clockCont
-    clockBoundingBox = document.createElement('div');
-    clockBoundingBox.style.top = `${clockCont.top}px`;
-    clockBoundingBox.style.left = `${clockCont.left}px`;
-    clockBoundingBox.style.width = `${clockCont.width}px`;
-    clockBoundingBox.style.height = `${clockCont.height}px`;
-
     // Create a bubble for the todo item
     createBubble(todo.text);
 
@@ -161,20 +122,6 @@ function createTodoElement(todo) {
         todos = todos.filter(t => t !== todo);
         li.remove();
         saveTodos();
-        clockCont = document.querySelector('#clock').getBoundingClientRect();
-        todoCont = document.querySelector('#todoContainer').getBoundingClientRect();
-        // Draw bounding box for todoCont
-        todoBoundingBox.style.top = `${todoCont.top}px`;
-        todoBoundingBox.style.left = `${todoCont.left}px`;
-        todoBoundingBox.style.width = `${todoCont.width}px`;
-        todoBoundingBox.style.height = `${todoCont.height}px`;
-
-        // Draw bounding box for clockCont
-        clockBoundingBox = document.createElement('div');
-        clockBoundingBox.style.top = `${clockCont.top}px`;
-        clockBoundingBox.style.left = `${clockCont.left}px`;
-        clockBoundingBox.style.width = `${clockCont.width}px`;
-        clockBoundingBox.style.height = `${clockCont.height}px`;
     });
 
     return li;
@@ -190,6 +137,21 @@ function addTodo(text) {
     const todoList = document.getElementById('todoList');
     todoList.appendChild(createTodoElement(todo));
     saveTodos();
+}
+
+function drawBoxes(){
+    // Draw bounding box for todoCont
+    todoBoundingBox.style.top = `${todoCont.top}px`;
+    todoBoundingBox.style.left = `${todoCont.left}px`;
+    todoBoundingBox.style.width = `${todoCont.width}px`;
+    todoBoundingBox.style.height = `${todoCont.height}px`;
+
+    // Draw bounding box for clockCont
+    clockBoundingBox = document.createElement('div');
+    clockBoundingBox.style.top = `${clockCont.top}px`;
+    clockBoundingBox.style.left = `${clockCont.left}px`;
+    clockBoundingBox.style.width = `${clockCont.width}px`;
+    clockBoundingBox.style.height = `${clockCont.height}px`;
 }
 
 // Initialize todo list
